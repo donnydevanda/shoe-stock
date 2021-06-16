3bc9a52576b23ccad47db6cb96eedbb548c78070
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-06-16 22:01:47 +0700

    statefull + overflow prevent

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -22,14 +22,18 @@ class DetailMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Header(),
-          Hero(shoe: shoe),
-          Info(shoe: shoe),
-          ButtonAdd(),
-        ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Header(),
+              Hero(shoe: shoe),
+              Info(shoe: shoe),
+              ButtonVote(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -38,34 +42,32 @@ class DetailMobile extends StatelessWidget {
 class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 0,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 14),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.arrow_back,
-              ),
+    return Container(
+      height: 80,
+      padding: EdgeInsets.symmetric(vertical: 14),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back,
             ),
-            Text(
-              "Details",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          Text(
+            "Details",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.share),
-            )
-          ],
-        ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.share),
+          )
+        ],
       ),
     );
   }
@@ -78,8 +80,8 @@ class Hero extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 3,
+    return Container(
+      height: 300,
       child: Image.asset(shoe.imageUrl),
     );
   }
@@ -92,64 +94,88 @@ class Info extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 5,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 24, horizontal: 14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              shoe.name,
+    return Container(
+      height: 500,
+      padding: EdgeInsets.symmetric(vertical: 24, horizontal: 14),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            shoe.name,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              "Rp. " + shoe.price.toString(),
               style: TextStyle(
-                fontSize: 30.0,
+                fontSize: 14,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Text(
-                "Rp. " + shoe.price.toString(),
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+          ),
+          Text(
+            shoe.description,
+            style: TextStyle(
+              fontSize: 16.0,
             ),
-            Text(
-              shoe.description,
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class ButtonAdd extends StatelessWidget {
+class ButtonVote extends StatefulWidget {
+  @override
+  _ButtonVoteState createState() => _ButtonVoteState();
+}
+
+class _ButtonVoteState extends State<ButtonVote> {
+  bool isCart = false;
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        padding: EdgeInsets.all(16),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.teal[900],
-            minimumSize: Size(80, 50),
-          ),
-          onPressed: () {},
-          child: Text(
-            "Add to Cart",
-            style: TextStyle(
-              fontSize: 14,
+    return Container(
+      height: 100,
+      padding: EdgeInsets.all(16),
+      child: !isCart
+          ? ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.teal[900],
+                minimumSize: Size(80, 50),
+              ),
+              onPressed: () {
+                setState(() {
+                  isCart = !isCart;
+                });
+              },
+              child: Text(
+                "Add to Cart",
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            )
+          : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[400],
+                minimumSize: Size(80, 50),
+              ),
+              onPressed: () {
+                setState(() {
+                  isCart = !isCart;
+                });
+              },
+              child: Text(
+                "Remove from cart",
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -6,17 +6,21 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.teal[100],
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Navbar(),
-            HeaderHorizontal(),
-            ShoeHorizontal(),
-            HeaderVertical(),
-            ShoeVertical(),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.teal[100],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Navbar(),
+                HeaderHorizontal(),
+                ShoeHorizontal(),
+                HeaderVertical(),
+                ShoeVertical(),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -26,23 +30,21 @@ class MainScreen extends StatelessWidget {
 class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        color: Colors.teal[600],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "ShoeStock",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w900,
-                color: Colors.white,
-              ),
+    return Container(
+      height: 50,
+      color: Colors.teal[600],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "ShoeStock",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -51,33 +53,30 @@ class Navbar extends StatelessWidget {
 class HeaderHorizontal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        padding: EdgeInsets.only(
-          top: 36,
-          right: 18,
-          left: 18,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Popular Shoes",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+    return Container(
+      padding: EdgeInsets.only(
+        top: 36,
+        right: 18,
+        left: 18,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "Popular Shoes",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              "More than thousand pairs sold every weeks.",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-              ),
+          ),
+          Text(
+            "More than thousand pairs sold every weeks.",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -86,59 +85,57 @@ class HeaderHorizontal extends StatelessWidget {
 class ShoeHorizontal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 12),
-        child: SizedBox(
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int index) {
-              final Shoe shoe = shoePopulars[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return DetailScreen(shoe: shoe);
-                    }),
-                  );
-                },
-                child: Container(
-                  width: 200,
-                  height: 150,
-                  child: Card(
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Image.asset(
-                            shoe.imageUrl,
+    return Container(
+      height: 200,
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: SizedBox(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            final Shoe shoe = shoePopulars[index];
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return DetailScreen(shoe: shoe);
+                  }),
+                );
+              },
+              child: Container(
+                width: 200,
+                height: 150,
+                child: Card(
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.asset(
+                          shoe.imageUrl,
+                        ),
+                        Text(
+                          shoe.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            shoe.name,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        ),
+                        Text(
+                          'Rp. ' + shoe.price.toString(),
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
                           ),
-                          Text(
-                            'Rp. ' + shoe.price.toString(),
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              );
-            },
-            itemCount: shoePopulars.length,
-          ),
+              ),
+            );
+          },
+          itemCount: shoePopulars.length,
         ),
       ),
     );
@@ -148,30 +145,27 @@ class ShoeHorizontal extends StatelessWidget {
 class HeaderVertical extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        padding: EdgeInsets.only(
-          top: 24,
-          left: 18,
-          right: 18,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Latest Shoes",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+    return Container(
+      padding: EdgeInsets.only(
+        top: 24,
+        left: 18,
+        right: 18,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "Latest Shoes",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              "New shoes give you new looks.",
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
+          ),
+          Text(
+            "New shoes give you new looks.",
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
       ),
     );
   }
@@ -180,68 +174,66 @@ class HeaderVertical extends StatelessWidget {
 class ShoeVertical extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 5,
-      child: Container(
-        padding: EdgeInsets.only(bottom: 24, left: 12, right: 12),
-        child: SizedBox(
-          child: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              final Shoe shoe = shoeNewests[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return DetailScreen(shoe: shoe);
-                    }),
-                  );
-                },
-                child: Card(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Image.asset(
-                          shoe.imageUrl,
+    return Container(
+      height: 550,
+      padding: EdgeInsets.only(bottom: 24, left: 12, right: 12),
+      child: SizedBox(
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            final Shoe shoe = shoeNewests[index];
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return DetailScreen(shoe: shoe);
+                  }),
+                );
+              },
+              child: Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(
+                        shoe.imageUrl,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 24,
+                          horizontal: 12,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              shoe.name,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Rp. ' + shoe.price.toString(),
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 24,
-                            horizontal: 12,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Text(
-                                shoe.name,
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                'Rp. ' + shoe.price.toString(),
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-              );
-            },
-            itemCount: shoeNewests.length,
-          ),
+              ),
+            );
+          },
+          itemCount: shoeNewests.length,
         ),
       ),
     );
