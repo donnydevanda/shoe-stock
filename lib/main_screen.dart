fab6ce795852815b9e7b46b67654eeaed6c53e01
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-06-16 16:39:31 +0700

    main screen & font

---
+++ lib/main_screen.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import './model/shoe.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.teal[100],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Navbar(),
+            HeaderHorizontal(),
+            ShoeHorizontal(),
+            HeaderVertical(),
+            ShoeVertical(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.teal[600],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "ShoeStock",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderHorizontal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 36),
+              child: Text(
+                "Popular Shoes",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                "More than thousand pairs sold every weeks.",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShoeHorizontal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: SizedBox(
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              final Shoe shoe = shoeList[index];
+              return Container(
+                width: 150,
+                height: 150,
+                child: Card(
+                  child: Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            shoe.name,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('Rp. ' + shoe.price.toString()),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: shoeList.length,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderVertical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 30,
+              left: 12,
+              right: 12,
+            ),
+            child: Text(
+              "Latest Shoes",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              "New shoes give you new looks.",
+              style: TextStyle(fontSize: 18),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ShoeVertical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: SizedBox(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              final Shoe shoe = shoeList[index];
+              return Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Image.network(
+                        shoe.imageUrl,
+                        height: 30,
+                        width: 30,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 24,
+                          horizontal: 12,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              shoe.name,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Rp. ' + shoe.price.toString(),
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+            itemCount: shoeList.length,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/shoe.dart
@@ -0,0 +1,79 @@
+class Shoe {
+  String name;
+  String description;
+  int price;
+  String imageUrl;
+
+  Shoe({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
+
+var shoeList = [
+  Shoe(
+      name: 'Nike KD 14',
+      description:
+          "The Nike KD 14 was released on April 17 and retails for 150. The popular midfoot strap makes a return after last being seen on the KD 7s. Multilayer mesh plus the aforementioned strap help reduce your foot’s movements inside the shoe. Full-length Zoom Air cushioning plus Cushlon foam give back energy for lasting performance.",
+      price: 2200000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/NikeKD14-Cropped-650x406.jpg'),
+  Shoe(
+      name: 'Nike LeBron 15',
+      description:
+          "LeBron’s 15th signature shoe, designed by Jason Petrie, features a next-generation Flyknit Battle construction and an advanced cushioning system that combines Max Air and Zoom Air units. The shoe also follows the recent trend of employing a full bootie construction to offer a secure and snug fit.",
+      price: 3000000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2017/09/lebron15-Cropped-768x480.jpg'),
+  Shoe(
+      name: 'Air Jordan 35',
+      description:
+          "The shoe will be headlined by Jordan Brand’s stable of big name athletes, including Zion Williamson, Jayson Tatum, Luka Doncic, Rui Hachimura, Kemba Walker, and Guo Ailun.",
+      price: 1800000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2019/12/AirJordan35-Cropped-650x406.jpg'),
+  Shoe(
+      name: 'Nike KD 14',
+      description:
+          "The Nike KD 14 was released on April 17 and retails for 150. The popular midfoot strap makes a return after last being seen on the KD 7s. Multilayer mesh plus the aforementioned strap help reduce your foot’s movements inside the shoe. Full-length Zoom Air cushioning plus Cushlon foam give back energy for lasting performance.",
+      price: 2200000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/NikeKD14-Cropped-650x406.jpg'),
+  Shoe(
+      name: 'Nike LeBron 15',
+      description:
+          "LeBron’s 15th signature shoe, designed by Jason Petrie, features a next-generation Flyknit Battle construction and an advanced cushioning system that combines Max Air and Zoom Air units. The shoe also follows the recent trend of employing a full bootie construction to offer a secure and snug fit.",
+      price: 3000000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2017/09/lebron15-Cropped-768x480.jpg'),
+  Shoe(
+      name: 'Air Jordan 35',
+      description:
+          "The shoe will be headlined by Jordan Brand’s stable of big name athletes, including Zion Williamson, Jayson Tatum, Luka Doncic, Rui Hachimura, Kemba Walker, and Guo Ailun.",
+      price: 1800000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2019/12/AirJordan35-Cropped-650x406.jpg'),
+  Shoe(
+      name: 'Nike KD 14',
+      description:
+          "The Nike KD 14 was released on April 17 and retails for 150. The popular midfoot strap makes a return after last being seen on the KD 7s. Multilayer mesh plus the aforementioned strap help reduce your foot’s movements inside the shoe. Full-length Zoom Air cushioning plus Cushlon foam give back energy for lasting performance.",
+      price: 2200000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/NikeKD14-Cropped-650x406.jpg'),
+  Shoe(
+      name: 'Nike LeBron 15',
+      description:
+          "LeBron’s 15th signature shoe, designed by Jason Petrie, features a next-generation Flyknit Battle construction and an advanced cushioning system that combines Max Air and Zoom Air units. The shoe also follows the recent trend of employing a full bootie construction to offer a secure and snug fit.",
+      price: 3000000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2017/09/lebron15-Cropped-768x480.jpg'),
+  Shoe(
+      name: 'Air Jordan 35',
+      description:
+          "The shoe will be headlined by Jordan Brand’s stable of big name athletes, including Zion Williamson, Jayson Tatum, Luka Doncic, Rui Hachimura, Kemba Walker, and Guo Ailun.",
+      price: 1800000,
+      imageUrl:
+          'https://ballershoesdb.com/wp-content/uploads/2019/12/AirJordan35-Cropped-650x406.jpg'),
+];
